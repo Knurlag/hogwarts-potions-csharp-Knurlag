6eb40ec451720f76256710111567a2913d115d84
Kristóf <bkristof54@gmail.com>
2022-09-13 17:07:59 +0200

    Added potioncontroller and views

---
+++ Controllers/PotionController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Controllers
+{
+    public class PotionController : Controller
+    {
+        private readonly HogwartsContext _context;
+
+        public PotionController(HogwartsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Potion
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.Potions =  _context.Potions.ToList();
+            ViewBag.Ingredients = _context.Ingredients.ToList();
+            return View();
+        }
+
+        // GET: Potion/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Potions == null)
+            {
+                return NotFound();
+            }
+
+            var potion = await _context.Potions
+                .Include(p=> p.Recipe.Ingredients)
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (potion == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Ingredients = potion.Recipe.Ingredients;
+            return View(potion);
+        }
+
+        // GET: Potion/Create
+        public IActionResult Create()
+        {
+            ViewBag.Ingredients = _context.Ingredients.ToList();
+            return View();
+        }
+
+        // POST: Potion/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,BrewingStatus")] Potion potion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(potion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(potion);
+        }
+
+        // GET: Potion/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Potions == null)
+            {
+                return NotFound();
+            }
+
+            var potion = await _context.Potions.FindAsync(id);
+            if (potion == null)
+            {
+                return NotFound();
+            }
+            return View(potion);
+        }
+
+        // POST: Potion/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ID,Name,BrewingStatus")] Potion potion)
+        {
+            if (id != potion.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(potion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PotionExists(potion.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(potion);
+        }
+
+        // GET: Potion/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Potions == null)
+            {
+                return NotFound();
+            }
+
+            var potion = await _context.Potions
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (potion == null)
+            {
+                return NotFound();
+            }
+
+            return View(potion);
+        }
+
+        // POST: Potion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Potions == null)
+            {
+                return Problem("Entity set 'HogwartsContext.Potions'  is null.");
+            }
+            var potion = await _context.Potions.FindAsync(id);
+            if (potion != null)
+            {
+                _context.Potions.Remove(potion);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PotionExists(long id)
+        {
+          return _context.Potions.Any(e => e.ID == id);
+        }
+    }
+}
