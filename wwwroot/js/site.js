397e0ca7c56efd38e622a7763fd4a33c79e848b5
Kristóf <bkristof54@gmail.com>
2022-09-14 16:22:55 +0200

    added some js for register and login

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+let loginButton = document.querySelector('#login');
+let registerButton = document.querySelector('#register');
+let loginForm = document.querySelector('#login-form');
+let registerForm = document.querySelector('#register-form');
+let warning = document.querySelector('#warning');
+loginButton.addEventListener('click', () => {
+    registerForm.setAttribute('hidden', 'hidden');
+    loginForm.removeAttribute('hidden');
+    warning.innerHTML = "";
+});
+registerButton.addEventListener('click', () => {
+    loginForm.setAttribute('hidden', 'hidden');
+    registerForm.removeAttribute('hidden');
+    warning.innerHTML = "";
+});
