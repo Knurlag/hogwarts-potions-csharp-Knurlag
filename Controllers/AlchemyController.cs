6e24e6b4cfe40bfa05341b91dd03ab05d783cbbc
Kristóf <bkristof54@gmail.com>
2022-09-28 15:55:17 +0200

    Added View and controller for classroom

---
+++ Controllers/AlchemyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    public class AlchemyController : Controller
+    {
+        [Route("Alchemy")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
