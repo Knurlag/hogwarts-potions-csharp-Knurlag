b6a98299117ac2c323bf1fe189254f43380018ca
Kristóf <bkristof54@gmail.com>
2022-09-28 15:57:12 +0200

    Added controller and view for recipes

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using HogwartsPotions.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly HogwartsContext _context;
+
+        public RecipesController(HogwartsContext context)
+        {
+            _context = context;
+        }
+        [Route("Recipes")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Recipes.Include(p => p.Ingredients).Include(m => m.Student).ToListAsync());
+        }
+    }
+}
