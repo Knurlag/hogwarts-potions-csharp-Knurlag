1766b153858c05de5492e4327e725c516809ea25
bkris <bkristof54@gmail.com>
2022-12-05 16:47:23 +0100

    Added ErrorHandlingFilter

---
+++ Helpers/ErrorHandlingFilter.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HogwartsPotions.Helpers;
+
+public class ErrorHandlingFilter : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+        ILog logger = LogManager.GetLogger("logger");
+        logger.Error(exception);
+
+        context.ExceptionHandled = true; //optional 
+    }
+}
