871439bb23372af47f802a76e5a08b7dbeeb5b77
bkris <bkristof54@gmail.com>
2022-12-06 12:40:47 +0100

    Generated test boilerplate for AlchemyController

---
+++ BackendTests/Controllers/AlchemyControllerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace BackendTests.Controllers
+{
+    [TestFixture]
+    public class AlchemyControllerTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private AlchemyController CreateAlchemyController()
+        {
+            return new AlchemyController();
+        }
+
+        [Test]
+        public void Index_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var alchemyController = this.CreateAlchemyController();
+
+            // Act
+            var result = alchemyController.Index();
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
