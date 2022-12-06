8775e7719bd3bbafd458889cc0cdb948b0629b99
bkris <bkristof54@gmail.com>
2022-12-06 12:39:43 +0100

    Generated test boilerplate for StudentController

---
+++ BackendTests/Controllers/StudentControllerTests.cs
@@ -0,0 +1,72 @@
+﻿namespace BackendTests.Controllers
+{
+    [TestFixture]
+    public class StudentControllerTests
+    {
+        private HogwartsContext subHogwartsContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.subHogwartsContext = Substitute.For<HogwartsContext>();
+        }
+
+        private StudentController CreateStudentController()
+        {
+            return new StudentController(
+                this.subHogwartsContext);
+        }
+
+        [Test]
+        public void Index_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var studentController = this.CreateStudentController();
+
+            // Act
+            var result = studentController.Index();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ValidateLogin_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var studentController = this.CreateStudentController();
+
+            // Act
+            var result = studentController.ValidateLogin();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Register_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var studentController = this.CreateStudentController();
+
+            // Act
+            var result = studentController.Register();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Logout_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var studentController = this.CreateStudentController();
+
+            // Act
+            var result = studentController.Logout();
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
