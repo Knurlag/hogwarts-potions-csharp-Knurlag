b0e6060be1d28c611908013b8aec30bcbcf019a0
bkris <bkristof54@gmail.com>
2022-12-06 12:40:35 +0100

    Generated test boilerplate for HomeController

---
+++ BackendTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace BackendTests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private HomeController CreateHomeController()
+        {
+            return new HomeController();
+        }
+
+        [Test]
+        public void Index_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var homeController = this.CreateHomeController();
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
