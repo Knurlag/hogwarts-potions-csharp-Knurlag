9f8e4da3b4844d34d2a016708857fcfaf7ac853f
bkris <bkristof54@gmail.com>
2022-12-06 12:39:21 +0100

    Generated boilerplate for DbInitializerTests

---
+++ BackendTests/Data/DbInitializerTests.cs
@@ -0,0 +1,39 @@
+﻿using HogwartsPotions.data;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace BackendTests.Data
+{
+    [TestFixture]
+    public class DbInitializerTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private DbInitializer CreateDbInitializer()
+        {
+            return new DbInitializer();
+        }
+
+        [Test]
+        public void Initialize_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var dbInitializer = this.CreateDbInitializer();
+            HogwartsContext context = null;
+
+            // Act
+            dbInitializer.Initialize(
+                context);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
