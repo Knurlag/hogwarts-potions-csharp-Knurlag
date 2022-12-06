87576c7b5718c28b9f50210e07181e6f3aa6b30f
bkris <bkristof54@gmail.com>
2022-12-06 12:40:16 +0100

    Generated test boilerplate for RecipeController

---
+++ BackendTests/Controllers/RecipesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace BackendTests.Controllers
+{
+    [TestFixture]
+    public class RecipesControllerTests
+    {
+        private HogwartsContext subHogwartsContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.subHogwartsContext = Substitute.For<HogwartsContext>();
+        }
+
+        private RecipesController CreateRecipesController()
+        {
+            return new RecipesController(
+                this.subHogwartsContext);
+        }
+
+        [Test]
+        public async Task Index_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var recipesController = this.CreateRecipesController();
+
+            // Act
+            var result = await recipesController.Index();
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
