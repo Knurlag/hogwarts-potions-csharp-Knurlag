1c229439a8fb293e37ee351b9530ad98c0213e8d
bkris <bkristof54@gmail.com>
2022-12-07 13:37:19 +0100

    Added 1 dbinitializer for all tests

---
+++ BackendTests/Initialize.cs
@@ -0,0 +1,16 @@
+﻿namespace BackendTests;
+
+public class Initialize
+{
+    public HogwartsContext HogwartsContext; 
+    public ISession session = new MockHttpSession();
+    public Initialize()
+    {
+        var options = new DbContextOptionsBuilder<HogwartsContext>().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HogwartsTest;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
+        this.HogwartsContext = new HogwartsContext(options);
+        DbInitializer.Initialize(HogwartsContext);
+
+        SessionHelper.SetObjectAsJson(session, "username", "tester");
+    }
+
+}
