1706e8ca0616fce5173e0cd6f2789dbe554e82e4
bkris <bkristof54@gmail.com>
2023-01-04 16:32:15 +0100

    Added a loginform class to send username and password to the context

---
+++ HogwartsPotions/Models/LoginForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Models;
+
+public class LoginForm
+{
+    public string Username;
+    public string Password;
+    public LoginForm(string username, string password)
+    {
+        this.Username = username;
+        this.Password = password;
+    }
+}
