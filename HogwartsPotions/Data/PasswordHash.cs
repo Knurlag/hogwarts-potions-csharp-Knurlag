7a2a597460645212f34397194e8f31bb44d41852
bkris <bkristof54@gmail.com>
2023-01-04 16:32:40 +0100

    Added a hash for passwords

---
+++ HogwartsPotions/Data/PasswordHash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HogwartsPotions.Data;
+
+public class PasswordHash
+{
+    public static string HashPassword(string password)
+    {
+        byte[] passwordHash;
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            passwordHash = sha256Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+        return Convert.ToBase64String(passwordHash);
+    }
+}
