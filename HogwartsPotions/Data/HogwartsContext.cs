a885c93c9048d977e3f8364dce522ceaa6a422e2
bkris <bkristof54@gmail.com>
2023-01-13 14:20:42 +0100

    Changed teble names and context now inherits from identitydbcontext

--- HogwartsPotions/Data/HogwartsContext.cs
+++ HogwartsPotions/Data/HogwartsContext.cs
@@ -8,12 +8,14 @@ using System.Xml.Linq;
 using HogwartsPotions.Models;
 using HogwartsPotions.Models.Entities;
 using HogwartsPotions.Models.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace HogwartsPotions.Data
 {
-    public class HogwartsContext : DbContext
+    public class HogwartsContext : IdentityDbContext
     {
         public int MaxIngredientsForPotions = 5;
         public DbSet<Student> Students { get; set; }
@@ -29,11 +31,43 @@ namespace HogwartsPotions.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().ToTable("Students");
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Students");
+            });
             modelBuilder.Entity<Room>().ToTable("Rooms");
             modelBuilder.Entity<Ingredient>().ToTable("Ingredients");
             modelBuilder.Entity<Recipe>().ToTable("Recipes");
             modelBuilder.Entity<Potion>().ToTable("Potions");
+            modelBuilder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role");
+            });
+            modelBuilder.Entity<IdentityUser>(entity =>
+            {
+                entity.ToTable(name: "Students");
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("StudentRoles");
+            });
+            modelBuilder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("StudentClaims");
+            });
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("StudentLogins");
+            });
+            modelBuilder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("RoleClaims");
+            });
+            modelBuilder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("StudentTokens");
+            });
 
         }
 
