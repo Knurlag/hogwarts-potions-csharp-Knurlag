39098b0104cf2a6ae01c38317d707ca55667c345
bkris <bkristof54@gmail.com>
2023-01-13 14:21:27 +0100

    changed registerform to fit new student

--- HogwartsPotions/Models/RegisterForm.cs
+++ HogwartsPotions/Models/RegisterForm.cs
@@ -5,16 +5,12 @@ namespace HogwartsPotions.Models;
 
 public class RegisterForm
 {
-    public string Username { get; }
-    public string Password { get; }
-    public HouseType HouseType { get; }
-    public PetType PetType { get; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public HouseType HouseType { get; set; }
+    public PetType PetType { get; set; }
 
-    public RegisterForm(string username, string password, HouseType houseType, PetType petType)
+    public RegisterForm()
     {
-        Username = username;
-        Password = password;
-        HouseType = houseType;
-        PetType = petType;
     }
 }
