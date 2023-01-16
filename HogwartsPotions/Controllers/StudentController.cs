ea4915b320cc00b4281bbfe087a9ad3b50d28918
bkris <bkristof54@gmail.com>
2023-01-16 14:04:29 +0100

    Added redirect if there are still cookies

--- HogwartsPotions/Controllers/StudentController.cs
+++ HogwartsPotions/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Mime;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,8 +30,12 @@ namespace HogwartsPotions.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            if (HttpContext.Request.Cookies.ContainsKey(".AspNetCore.Identity.Application"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
             ViewBag.HouseTypes = new HouseType[] {HouseType.Gryffindor, HouseType.Hufflepuff, HouseType.Ravenclaw, HouseType.Slytherin};
             ViewBag.PetTypes = new PetType[] {PetType.Cat, PetType.Owl, PetType.Rat, PetType.None};
             return View();
@@ -37,7 +43,7 @@ namespace HogwartsPotions.Controllers
         public async Task<IActionResult> ValidateLogin(LoginForm loginForm)
         {
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-            var result = await _signInManager.PasswordSignInAsync(loginForm.Username, loginForm.Password, isPersistent: false, lockoutOnFailure: false);
+            var result = await _signInManager.PasswordSignInAsync(loginForm.Username, loginForm.Password, isPersistent: true, lockoutOnFailure: false);
             if (result.Succeeded)
                 {
                     SessionHelper.SetObjectAsJson(HttpContext.Session, "username", loginForm.Username);
@@ -54,7 +60,7 @@ namespace HogwartsPotions.Controllers
 
             Student user = new Student() { UserName = registerForm.Username, HouseType = registerForm.HouseType, PetType = registerForm.PetType};
             var result = await _userManager.CreateAsync(user, registerForm.Password);
-            var student = await _userManager.FindByNameAsync(user.UserName);
+            Student student = await _userManager.FindByNameAsync(user.UserName);
             await _userManager.AddToRoleAsync(student, "Student");
             if (result.Succeeded)
             {
