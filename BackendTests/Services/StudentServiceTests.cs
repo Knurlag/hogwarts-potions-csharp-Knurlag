98ca1d41708495d61b6053bd5201501df287be31
bkris <bkristof54@gmail.com>
2023-01-18 18:42:44 +0100

    Added tests for StudentService

---
+++ BackendTests/Services/StudentServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using HogwartsPotions.Data;
+using HogwartsPotions.Services;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace BackendTests.Services
+{
+    [TestFixture]
+    public class StudentServiceTests
+    {
+        private HogwartsContext subHogwartsContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.subHogwartsContext = new Initialize().HogwartsContext;
+        }
+
+        private StudentService CreateService()
+        {
+            return new StudentService(
+                this.subHogwartsContext);
+        }
+
+        [Test]
+        public void GetStudent_Test()
+        {
+            // Arrange
+            var service = this.CreateService();
+            string username = "Carson Alexander";
+
+            // Act
+            var result = service.GetStudent(
+                username);
+
+            // Assert
+            Assert.That(result.UserName, Is.EqualTo("Carson Alexander"));
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            subHogwartsContext.Database.EnsureDeleted();
+        }
+    }
+}
